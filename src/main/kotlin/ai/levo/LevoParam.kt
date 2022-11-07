2b0cbd98dad5b48f8f317a8c9c0ce7dc31836514
ricekot <github@ricekot.com>
2022-11-07 16:16:34 +0530

    Remove unused configuration option

--- src/main/kotlin/ai/levo/LevoParam.kt
+++ src/main/kotlin/ai/levo/LevoParam.kt
@@ -8,7 +8,6 @@ private const val PARAM_BASE_KEY = "levoai"
 private const val PARAM_CURRENT_VERSION: Int = 1
 private const val PARAM_ENABLED = "$PARAM_BASE_KEY.enabled"
 private const val PARAM_SATELLITE_URL = "$PARAM_BASE_KEY.satelliteUrl"
-private const val PARAM_CONFIRM_ON_REMOVE = "$PARAM_BASE_KEY.confirmOnRemove"
 
 class LevoParam : VersionedAbstractParam(), Logging {
 
@@ -24,12 +23,6 @@ class LevoParam : VersionedAbstractParam(), Logging {
             config.setProperty(PARAM_SATELLITE_URL, value)
         }
 
-    private var confirmOnRemove: Boolean = true
-        set(value) {
-            field = value
-            config.setProperty(PARAM_CONFIRM_ON_REMOVE, value)
-        }
-
     override fun getConfigVersionKey(): String = PARAM_BASE_KEY + VERSION_ATTRIBUTE
     override fun getCurrentVersion(): Int = PARAM_CURRENT_VERSION
     override fun updateConfigsImpl(fileVersion: Int) {}
@@ -38,7 +31,6 @@ class LevoParam : VersionedAbstractParam(), Logging {
         try {
             enabled = config.getBoolean(PARAM_ENABLED, enabled)
             satelliteUrl = config.getString(PARAM_SATELLITE_URL, satelliteUrl)
-            confirmOnRemove = config.getBoolean(PARAM_CONFIRM_ON_REMOVE, confirmOnRemove)
         } catch (e: ConversionException) {
             logger.error("Error parsing levoai configuration: ${e.message}", e)
         }
