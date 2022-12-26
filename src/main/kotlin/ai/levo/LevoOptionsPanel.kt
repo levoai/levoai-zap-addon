60d540b33ec89d4e8f6a9137941db3221c92e8d3
ricekot <github@ricekot.com>
2022-12-26 23:09:10 +0530

    Validate the Satellite URL structure when it is modified

--- src/main/kotlin/ai/levo/LevoOptionsPanel.kt
+++ src/main/kotlin/ai/levo/LevoOptionsPanel.kt
@@ -15,6 +15,7 @@
  */
 package ai.levo
 
+import org.apache.commons.httpclient.URI
 import org.parosproxy.paros.Constant
 import org.parosproxy.paros.model.OptionsParam
 import org.parosproxy.paros.view.AbstractParamPanel
@@ -22,6 +23,7 @@ import org.zaproxy.zap.utils.ZapTextField
 import org.zaproxy.zap.view.LayoutHelper
 import java.awt.GridBagLayout
 import java.awt.Insets
+import java.net.URL
 import javax.swing.JLabel
 
 private val NAME = Constant.messages.getString("levoai.options.panelName")
@@ -45,6 +47,15 @@ class LevoOptionsPanel : AbstractParamPanel() {
     override fun initParam(obj: Any?) {
         val levoParam = (obj as OptionsParam).getParamSet(LevoParam::class.java)
         satelliteUrl.text = levoParam.satelliteUrl
+    }
+
+    override fun validateParam(obj: Any?) {
+        try {
+            URL(satelliteUrl.text).toURI()
+            URI(satelliteUrl.text, true)
+        } catch (e: Exception) {
+            throw IllegalArgumentException(Constant.messages.getString("levoai.error.invalidUrl", e.localizedMessage))
+        }
     }
 
     override fun saveParam(obj: Any?) {
